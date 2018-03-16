4686b97b082c5feb1dcd99992a56bdc5eefcf30f
squinto13 <james.ward.antony@gmail.com>
2018-03-16 10:03:40 -0400

    Add files via upload
    Note that these are still with the matrices as [25 x 25 x 2 x 2]. I will work on changing that ASAP and I'll commit when done!

--- runTestStudyExp.m
+++ runTestStudyExp.m
@@ -15,61 +15,136 @@
 % when retrieving fruit-apple, then fruit-pair gets weakened relative to control
 % when re-studying fruit-pair then connection get's strengthened again but the connection between apple and pear may weaken
 
-
+rng('shuffle');reset(RandStream.getGlobalStream,sum(100*clock));
 % initialize memory network
-
-% number of units
+clear;
+%number of pairs
+Npairs=25;
+% number of units per pair
 Nunits = 2;
-
+inhibition=0.1;
 % initial activation
 initAct = zeros(Nunits, 1);
-
+wt_init=0.01;%higher initial weights -> more noise
+gain_init=0.1;
+tau_init=0.00157;
 % weights
-initWeightScale = 0.01;
-W = (ones(Nunits) - eye(Nunits)) * initWeightScale;
+for i=1:Npairs;
+    for j=1:Npairs;
+        wt_adj=rand(1)*wt_init*5;%+wt_init;
+        initWeightScale = wt_init+wt_adj(1);
+        Ws(i,j,:,:) = (ones(Nunits) - eye(Nunits)) * initWeightScale;
+        wt_adj=rand(1)*wt_init*5;%+wt_init;
+        initWeightScale = wt_init+wt_adj(1);
+        Wt(i,j,:,:) = (ones(Nunits) - eye(Nunits)) * initWeightScale;
+    end;
+    gains_s(i)=rand(1)+gain_init;
+    gains_t(i)=rand(1)+gain_init;
+    taus_s(i)=randn(1)*tau_init/10+tau_init;
+    taus_t(i)=randn(1)*tau_init/10+tau_init;
+end;
+
+W_study=Ws;W_test=Wt;
+
+% for later
+%here we reconfigure weights to be the activation given the ROW of each
+%corresponding column node. should be strongest for re-learned item
+W_study2=squeeze(squeeze(Ws(:,:,2,1)));W_study3=W_study2;
+W_test2=squeeze(squeeze(Wt(:,:,2,1)));W_test3=W_test2;
 
 % activation threshold
 threshold = 0.4;
 
-% run study trial
+%study ALL items, both re-studied later and tested later
 externalInput_study = 1 * ones(Nunits, 1);
-memoryNet_study = simpleMemoryNet(W, initAct, threshold);
-activation_log_study = memoryNet_study.runTrialUntilThreshold(externalInput_study, Nunits);
-W_study = memoryNet_study.adjustWeights();
+for i=1:Npairs
+    %strengthen corresponding unit via study
+    memoryNet_study = simpleMemoryNet(squeeze(Ws(i,i,:,:)),initAct,threshold,gains_s(i),taus_s(i),inhibition);
+    activation_log_study = memoryNet_study.runTrialUntilThreshold(externalInput_study, Nunits);
+    W_study(i,i,:,:) = memoryNet_study.adjustWeights();
+    W_study2(i,i)=squeeze(W_study(i,i,2,1));
+    memoryNet_test = simpleMemoryNet(squeeze(Wt(i,i,:,:)),initAct,threshold,gains_t(i),taus_t(i),inhibition);
+    activation_log_study = memoryNet_test.runTrialUntilThreshold(externalInput_study, Nunits);
+    W_test(i,i,:,:) = memoryNet_test.adjustWeights();
+    W_test2(i,i)=squeeze(W_test(i,i,2,1));
+end;
+%at this point, W_study2 / W_test2 should be similar.
+
+% run re-study trial
+externalInput_study = 1 * ones(Nunits, 1);
+for i=1:Npairs
+    %strengthen corresponding unit via study
+    memoryNet_study = simpleMemoryNet(squeeze(W_study(i,i,:,:)),initAct,threshold,gains_s(i),taus_s(i),inhibition);
+    activation_log_study = memoryNet_study.runTrialUntilThreshold(externalInput_study, Nunits);
+    study_a{i}.log=activation_log_study;
+    W_study(i,i,:,:) = memoryNet_study.adjustWeights();
+    W_study3(i,:)=squeeze(W_study(i,:,2,1));%1,1
+end;
+%figure;subplot(211);imagesc(W_study2);colorbar;
+%subplot(212);imagesc(W_study3);colorbar;
 
 % run test trial
 externalInput_test = 1 * ones(Nunits, 1);
 externalInput_test(2:end) = 0;
-memoryNet_test = simpleMemoryNet(W, initAct, threshold);
-activation_log_test= memoryNet_test.runTrialUntilThreshold(externalInput_test, Nunits);
-W_test = memoryNet_study.adjustWeights();
+for i=1:Npairs
+    memoryNet_test = simpleMemoryNet(squeeze(W_test(i,i,:,:)),initAct,threshold,gains_t(i),taus_t(i),inhibition);
+    activation_log_test= memoryNet_test.runTrialUntilThreshold(externalInput_test, Nunits);
+    test_a{i}.log=activation_log_test;
+    [W_test(i,i,:,:),fract] = memoryNet_test.adjustWeights();
+    %cycle through competitors and implement RIF
+    for j=1:Npairs
+        if i~=j
+            %memoryNet_test = simpleMemoryNet(squeeze(W_test(i,j,:,:)),initAct,threshold,gains_t(i),taus_t(i),inhibition);
+            %activation_log_test= memoryNet_test.runTrialUntilThreshold(externalInput_test, Nunits);
+            %W_test(i,j,:,:) = memoryNet_test.weaken();
+            W_test(i,j,:,:)=W_test(i,j,:,:)/((fract-1)/2+1);
+        end;
+    end;
+    %adjust weights for both target and competitor
+    W_test3(i,:)=squeeze(W_test(i,:,2,1));%2,1
+end;
 
-% plot activations
+%W_study3(W_study3<0)=0;%zero out negative weights
+%W_test3(W_test3<0)=0;%zero out negative weights
+
+%% plot
+%activations
 all_activation = [activation_log_study(:); activation_log_test(:)];
-subplot(2,2,1);
-plot(activation_log_study);
-ylim([min(all_activation) max(all_activation)]);
-xlabel('time');
-ylabel('unit activation');
-title('study condition');
-subplot(2,2,2);
-plot(activation_log_test);
-ylim([min(all_activation) max(all_activation)]);
-xlabel('time');
-ylabel('unit activation');
-title('test condition');
+yl=[min(all_activation) max(all_activation)];
+figure;subplot(321);
+for i=1:length(study_a);plot(study_a{1,i}.log);hold on;end;
+ylim(yl);xlabel('time');ylabel('unit activation');title('study condition');
+subplot(322);
+for i=1:length(test_a);plot(test_a{1,i}.log);hold on;end;
+ylim(yl);xlabel('time');ylabel('unit activation');title('test condition');
+%  weight matrix
+all_ws=[W_study2(:);W_test2(:);W_study3(:);W_test3(:)];
+yl=[0, max(all_ws)];%yl=[min(all_ws), max(all_ws)];
+subplot(323);imagesc(W_study2);colorbar;caxis(yl);
+title('Post-learning study weights');xlabel('Input');ylabel('Output');
+subplot(324);imagesc(W_test2);colorbar;caxis(yl);
+title('Post-learning test weights');xlabel('Input');ylabel('Output');
+subplot(325);imagesc(W_study3);colorbar;caxis(yl);
+title('Post-restudy weights');xlabel('Input');ylabel('Output');
+subplot(326);imagesc(W_test3);colorbar;caxis(yl);
+title('Post-test weights');xlabel('Input');ylabel('Output');
 
-% plot weight matrix
-allWeights = [W_study(:); W_test(:)];
-subplot(2,2,3);
-imagesc(W_study);
-colorbar;
-caxis([min(allWeights), max(allWeights)]);
-title('weights after training');
-subplot(2,2,4);
-imagesc(W_test);
-colorbar;
-title('weights after training');
-caxis([min(allWeights), max(allWeights)]);
+%% implement forgetting over time!
+%define memory as a thresholded signal:noise ratio
+init_sn_study=zeros(Npairs,1);init_sn_test=init_sn_study
+for i=1:Npairs
+    %init_sn_study(i)=W_study3(i,i)/sum(W_study3(1:Npairs~=i,i));
+    %init_sn_test(i)=W_test3(i,i)/sum(W_test3(1:Npairs~=i,i));
+    init_sn_study(i)=W_study3(i,i)/sum(W_study3(:,i));
+    init_sn_test(i)=W_test3(i,i)/sum(W_test3(:,i));
+end;
+
+allsn=[init_sn_study;init_sn_test];
+snspace=linspace(min(allsn),max(allsn),10);
+figure;h=histogram(init_sn_study,snspace,'FaceColor',[0 0 1]);hold on;
+histogram(init_sn_test,snspace,'FaceColor',[1 0 0]);hold off;
+
+
+
 
 
--- simpleMemoryNet.m
+++ simpleMemoryNet.m
@@ -5,22 +5,29 @@ classdef simpleMemoryNet < handle
         W; % weight matrix
         init; % initial activation value
         gain; % gain of activation function
+        W_gain; %gain of weights
         
         tau; % integration constant
         threshold; % response threshold
         maxTimeSteps; % maximum amount of simulation time steps
+        actFinish;
         
         activation_log; % log of activation for all units
         activation; % current activation
+        
+        inhibition;
     end
     
     methods
         
         % constructor
-        function this = simpleMemoryNet(W_arg, init_arg, threshold_arg)
+        function this = simpleMemoryNet(W_arg,init_arg,threshold_arg,gain_arg,tau_arg,inhib_arg)
             this.W = W_arg;
             this.init = init_arg;
             this.threshold = threshold_arg;
+            this.tau=tau_arg;
+            this.inhibition = inhib_arg;
+            this.gain=gain_arg;
             
             if(length(this.init) ~= size(this.W))
                 error('Dimension of initial activation vector does not match dimension of weight matrix.');
@@ -31,10 +38,11 @@ classdef simpleMemoryNet < handle
             end
             
             % default parameters
-            this.gain = 1;
-            this.tau = 0.01;
+            %JWA
+            %this.gain = 1;
+            %this.tau = 0.0015;%0.01
             this.maxTimeSteps = 1000;
-            
+            this.W_gain=0.0005;
         end
         
         
@@ -62,50 +70,76 @@ classdef simpleMemoryNet < handle
                 this.activation_log = [this.activation_log; transpose(this.activation)];
             end
             
-            activation_log = this.activation_log;
+            if(sum(this.activation > this.threshold) >= N_threshold)
+                this.actFinish=1;
+            else;this.actFinish=0;
+            end;
             
+            activation_log = this.activation_log;
         end
         
         % compute activation at the next time step
         function newAct = computeNewActivation(this, input)
-            
+            %JWA
+            gain_adj=randn(1);%/5
+            trial_gain = this.gain+gain_adj;
             if(size(input,1) == 1)
                 input = transpose(input);
             end
             
             netInput = this.W * this.activation + input;
-            newAct = this.activation + this.tau * (-this.activation + 1./(1+exp(-this.gain*netInput)));
-            
+            newAct = this.activation + this.tau * (-this.activation + 1./(1+exp(-trial_gain*netInput)));
         end
         
         % adjust weights
-        function W = adjustWeights(this)
+        function [W,fract] = adjustWeights(this)
             
-            W_delta = zeros(size(this.W));
-            
-            % compute weight adjustments over time
-            for t = 1:size(this.activation_log,1)
-                current_activation = this.activation_log(t,:);
-                W_delta = W_delta + this.computeWeightAdjustment(current_activation);
-            end
-            
-            % apply weight adjustment
-            W = this.W + W_delta;
-            this.W = W;
-            
+            %JWA
+            %adjust weights if activation finished
+            if this.actFinish 
+                W_delta = zeros(size(this.W));
+                
+                % compute weight adjustments over time
+                for t = 1:size(this.activation_log,1)
+                    current_activation = this.activation_log(t,:);
+                    W_delta = W_delta + this.computeWeightAdjustment(current_activation)*this.W_gain;
+                end
+                
+                % apply weight adjustment
+                W = this.W + W_delta;
+                fract=W(1,2)/this.W(1,2);
+                this.W = W;
+            else;W = this.W;fract=1;
+            end;
         end
         
         function  W_delta = computeWeightAdjustment(this, activation)
             
-            % make sure activation vetor is column vector
+            % make sure activation vector is column vector
             if(size(activation,1) == 1)
                 activation = transpose(activation);
             end
             Nunits = length(activation);
             
+            %JWA
             W_delta = (ones(Nunits) - eye(Nunits)) .* (transpose(activation) * activation);
         end
         
+        function W = weaken(this)
+            %JWA
+            W_delta = zeros(size(this.W));
+            
+            % compute weight adjustments over time
+            for t = 1:size(this.activation_log,1)
+                current_activation = this.activation_log(t,:);
+                W_delta = W_delta + this.computeWeightAdjustment(current_activation)*this.W_gain;
+            end
+            
+            % apply weight adjustment
+            W = this.W-W_delta;
+            this.W = W;
+        end
+        
+        
     end
-    
 end
